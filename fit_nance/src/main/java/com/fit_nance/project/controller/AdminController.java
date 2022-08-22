67dff8aee8f9688fd6e79da59a57158b4cfe4f73
silverkong <bina25@nate.com>
2022-08-22 17:30:42 +0900

    add admin login setting

---
+++ fit_nance/src/main/java/com/fit_nance/project/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.fit_nance.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+	
+	@RequestMapping("/admin")
+	public String viewLogin() {
+		return "admin/login";
+	}
+}
