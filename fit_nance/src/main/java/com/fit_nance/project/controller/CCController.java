ec53ca7b03085aa9ac5ca6a4553d91d92ed8adda
silverkong <bina25@nate.com>
2022-08-25 15:50:26 +0900

    modify notice 공지사항 전체 조회

--- fit_nance/src/main/java/com/fit_nance/project/controller/CCController.java
+++ fit_nance/src/main/java/com/fit_nance/project/controller/CCController.java
@@ -1,13 +1,26 @@
 package com.fit_nance.project.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fit_nance.project.model.NoticeVO;
+import com.fit_nance.project.service.NoticeService;
 
 @Controller
 public class CCController {
 	
+	@Autowired
+	NoticeService nService;
+	
 	@RequestMapping("/notice")
-	public String viewNotice() {
+	public String viewNotice(Model model) {
+		ArrayList<NoticeVO> noticeList = nService.listAllNotice();
+		model.addAttribute("noticeList", noticeList);
+		
 		return "cc/cc_notice";
 	}
 	
