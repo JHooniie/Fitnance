4989310413a01032eb41641a5bae834c0f5106c9
gantry8764 <gantry8764@gmail.com>
2022-09-03 20:49:39 +0900

    add calc_rpayment files

--- fit_nance/src/main/java/com/fit_nance/project/controller/CalculatorController.java
+++ fit_nance/src/main/java/com/fit_nance/project/controller/CalculatorController.java
@@ -1,7 +1,14 @@
 package com.fit_nance.project.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fit_nance.project.model.Calc_rpayVO;
 
 @Controller
 public class CalculatorController {
@@ -9,5 +16,66 @@ public class CalculatorController {
 	@RequestMapping("/cal_tax")
 	public String viewNotice() {
 		return "calculator/cal_tax";
+	}
+	
+	// 대출이자 계산기
+	
+	@RequestMapping("/calc_rpayment") 
+	public String calc_rpayment(Model model) {
+		return "calculator/calc_rpayment";
+	}
+	 
+	@ResponseBody
+	@RequestMapping("/calc_rpay")
+	public /*String*/ ArrayList<Calc_rpayVO> /*ModelAndView*/ calc_rpay(Model model
+			, @RequestParam(value="calc_lend_loan") int calc_lend_loan
+			, @RequestParam(value="calc_dly_rate_year") double calc_dly_rate_year
+			, @RequestParam(value="calc_rpay_period") int calc_rpay_period
+			//, @RequestParam(value="c1") int calc_lend_loan
+			//, @RequestParam(value="c2") double calc_dly_rate_year
+			//, @RequestParam(value="c3") int calc_rpay_period
+			) {
+		System.out.println("대출 금액 : "+calc_lend_loan);
+		System.out.println("연 이자율 : "+calc_dly_rate_year);
+		System.out.println("상환 기간 : "+calc_rpay_period);
+		
+		// 상환 개월 수 = 상환기간*12
+		int calc_rpay_month = calc_rpay_period * 12;
+		//System.out.println("상환 개월 수 : "+calc_rpay_month);
+		
+		// 월 이자율 = 연 이자율 / 12
+		double calc_dly_rate_month = Math.round(calc_dly_rate_year / 12 * 1000000) / 1000000.0;
+		//System.out.println("월 이자율 : "+calc_dly_rate_month);
+		double result1 = calc_lend_loan * (calc_dly_rate_month / 100.0) * Math.pow((1 + calc_dly_rate_month / 100.0), calc_rpay_month);
+		double result2 = Math.pow((1 + calc_dly_rate_month / 100.0), calc_rpay_month) - 1;
+		int result = (int) Math.round(result1 / result2);
+		
+		int money = calc_lend_loan;// 잔금 1,000만 원
+		int total = money;
+		int rpay = result;// 매 월 상환금 438,714 원
+		int rate = (int)(total * calc_dly_rate_month / 100);// 이자
+		int rpay_month = rpay - rate;
+		
+		ArrayList<Calc_rpayVO> list_rpay = new ArrayList<Calc_rpayVO>();
+		
+		for(int i=1; i<calc_rpay_month+1; i++) {
+			Calc_rpayVO vo = new Calc_rpayVO();
+			rate = (int)(total * calc_dly_rate_month / 100);
+			rpay_month = rpay - rate;
+			total -= rpay_month;
+			if(total<0) {
+				rpay_month -= total;
+				rate -= total;
+				total = 0;
+			}
+			vo.setIndex(i);
+			vo.setRpay_month(rpay_month);
+			vo.setRpay_rate(rate);
+			vo.setRpay(rpay);
+			vo.setTotal(total);
+			list_rpay.add(vo);
+			System.out.println("회차 : "+i+"	납입 원금 : " + rpay_month + "	대출 이자 : " + rate + "	월 상환금 : " + rpay+"대출잔금 : " + total);
+		}
+		return list_rpay;
 	}
 }
--- fit_nance/src/main/resources/static/js/calc_rpayment.js
+++ fit_nance/src/main/resources/static/js/calc_rpayment.js
@@ -155,6 +155,7 @@
 	
 	{// 계산하기
 		$('.btn_calc').click(function(){
+			
 			if($('.input_lend_loan').val() == ""){
 				alert("대출 금액을 입력해주세요");
 				return false;
@@ -164,10 +165,83 @@
 			} else if($('.input_rpay_period').val() == ""){
 				alert("상환기간을 입력해주세요");
 				return false;
-			} else if($('.input_grace_period').val() == ""){
-				alert("거치기간을 입력해주세요");
-				return false;
 			}
+			
+			// 대출 금액 *
+			var calc_lend_loan = Number($('.input_lend_loan').val().replace(/[^\d]+/g, ""));
+			// 연 이자율
+			var calc_dly_rate_year = Number($('.input_dly_rate').val());
+			// 상환기간
+			var calc_rpay_period = Number($('.input_rpay_period').val());
+			
+			$('.c1').val(calc_lend_loan);
+			$('.c2').val(calc_dly_rate_year);
+			$('.c3').val(calc_rpay_period);
+			var first_rate_month;
+			var first_lend_month;
+			
+			
+			$.ajax({
+            	url: "calc_rpay",
+            	type: "post",
+            	data: {
+            			calc_lend_loan: calc_lend_loan,
+            			calc_dly_rate_year: calc_dly_rate_year,
+            			calc_rpay_period: calc_rpay_period
+            	},
+            	success: function(result){
+            		// 계산 결과
+					$('.calc_rpay_option').text($('.select_rpay_type').val());
+					$('.calc_lend_loan').text($('.input_lend_loan').val());
+					$('.calc_rpay_period').text($('.input_rpay_period').val());
+					$('.calc_dly_rate').text($('.input_dly_rate').val());
+            		$('.calc_rpay_result').text(((result[0].rpay).toLocaleString()).replace( /(\d)(?=(?:\d{3})+(?!\d))/g, '$1,' ));
+            		console.log(result);
+            		$('#table .data').remove();
+            		var idx;
+            		var total_dly = 0;
+            		if(calc_rpay_period<6){
+            			idx = calc_rpay_period * 2;
+            		} else {
+            			idx = 12;
+            		}
+            		
+            		var rs_rpay_rate = ((result[0].rpay_rate).toLocaleString()).replace( /(\d)(?=(?:\d{3})+(?!\d))/g, '$1,' );
+        			var rs_rpay_month = ((result[0].rpay_month).toLocaleString()).replace( /(\d)(?=(?:\d{3})+(?!\d))/g, '$1,' );
+        			var rs_rpay = ((result[0].rpay).toLocaleString()).replace( /(\d)(?=(?:\d{3})+(?!\d))/g, '$1,' );
+            		
+            		$('#table').append("<tr class="+"data"+"><td class="+"index"+">"+result[0].index+"</td><td>"+rs_rpay_rate+"원</td><td>"+rs_rpay_month+"원</td><td>"+rs_rpay+"원</td></tr>");
+            		for(var i=idx-1; i<result.length; i+=idx){
+            			rs_rpay_rate = ((result[i].rpay_rate).toLocaleString()).replace( /(\d)(?=(?:\d{3})+(?!\d))/g, '$1,' );
+            			rs_rpay_month = ((result[i].rpay_month).toLocaleString()).replace( /(\d)(?=(?:\d{3})+(?!\d))/g, '$1,' );
+            			rs_rpay = ((result[i].rpay).toLocaleString()).replace( /(\d)(?=(?:\d{3})+(?!\d))/g, '$1,' );
+            			$('#table').append("<tr class="+"data"+"><td class="+"index"+">"+result[i].index+"</td><td>"+rs_rpay_rate+"원</td><td>"+rs_rpay_month+"원</td><td>"+rs_rpay+"원</td></tr>");
+            			//$('#table').append("<tr class="+"data"+"><td class="+"index"+">"+result[i].index+"</td><td>"+result[i].rpay_rate+"원</td><td>"+result[i].rpay_month+"원</td><td>"+result[i].rpay+"원</td></tr>");
+            		}
+            		result.forEach(function(el, index){
+            			console.log(Number(el.rpay_rate));
+						total_dly += el.rpay_rate;
+            		});
+            		$('.total_rpay_rate').text(((total_dly).toLocaleString()).replace( /(\d)(?=(?:\d{3})+(?!\d))/g, '$1,' ));
+                },
+                error:function(request,status,error){
+                    console.log("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+                }
+            });
+			
+			// 원리금분할상환
+			if($('.select_rpay_type').val() == "원리금분할상환"){
+			
+			}
+			// 원금분할상환
+			else if($('.select_rpay_type').val() == "원금분할상환"){
+			
+			}
+			// 만기일시상환
+			else {
+			
+			}
+			
 			
 		});
 	}
