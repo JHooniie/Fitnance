2d9222598bf91892388264a499791c9b83424c64
gantry8764 <gantry8764@gmail.com>
2022-09-17 23:16:52 +0900

    add admin_QnA files

--- fit_nance/src/main/java/com/fit_nance/project/dao/IQNADAO.java
+++ fit_nance/src/main/java/com/fit_nance/project/dao/IQNADAO.java
@@ -7,8 +7,32 @@ import com.fit_nance.project.model.QNAVO;
 public interface IQNADAO {
 	public void insertQNA(String qTitle, String qContent, String memId);
 	public ArrayList<QNAVO> listMemQNA(String memId); 
-	//public ArrayList<QNAVO> questionMemQNA(int qIndex);
 	public QNAVO questionMemQNA(int qIndex);
-	//public ArrayList<QNAVO> answerMemQNA(String memId, int qIndex);
 	public QNAVO answerMemQNA(String memId, int qIndex);
+	
+	
+	// admin
+	// QNA 리스트 전체 조회
+	public ArrayList<QNAVO> listAllQNA();
+	
+	// QNA 문의 조회
+	public QNAVO selectQuestion(int qIndex);
+	
+	// QNA 답변 조회
+	public QNAVO selectAnswer(int qIndex);
+	
+	// QNA 상세 조회
+	public QNAVO detailViewQNA(int qIndex);
+	
+	// QNA 답변 등록
+	public void insertAnswer(int qIndex, String aTitle, String aContent);
+	
+	// QNA 답변 수정
+	public void updateAnswer(int qIndex, String aTitle, String aContent);
+	
+	// QNA 게시글 삭제
+	// 문의 사항
+	public void deleteQuestion(int qIndex);
+	// 문의 답변
+	public void deleteAnswer(int qIndex);
 }
--- fit_nance/src/main/webapp/WEB-INF/views/admin/layout/admin_common.jsp
+++ fit_nance/src/main/webapp/WEB-INF/views/admin/layout/admin_common.jsp
@@ -59,7 +59,7 @@
 
     <!-- 문의사항 -->
     <div id="navQnA" class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+        <a class="nav-link collapsed" href="<c:url value='/admin/qna'/>" data-toggle="collapse" data-target="#collapsePages"
             aria-expanded="true" aria-controls="collapsePages">
             <i class="fa-solid fa-comments"></i>
             <span>문의사항</span>
