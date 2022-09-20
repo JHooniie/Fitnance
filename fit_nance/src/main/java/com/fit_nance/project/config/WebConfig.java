1c86b9445dab522a5680445f46462c222a4209a7
JHooniie <cliffear324@gmail.com>
2022-09-21 03:50:18 +0900

    hotfix config

--- fit_nance/src/main/java/com/fit_nance/project/config/WebConfig.java
+++ fit_nance/src/main/java/com/fit_nance/project/config/WebConfig.java
@@ -3,18 +3,23 @@ package com.fit_nance.project.config;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan(basePackages = {"com.fit_nance.project"})
 @MapperScan(basePackages = {"com.fit_nance.project"})
+@PropertySources({@PropertySource(value="file:c:/springWorkspace/configure.properties",ignoreResourceNotFound=true),
+	@PropertySource(value="file:/usr/local/project/properties/configure.properties",ignoreResourceNotFound=true),
+	@PropertySource(value="file:/usr/local/project/properties/application-oauth.properties",ignoreResourceNotFound=true)})
 public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("images/**")
-				.addResourceLocations("file:///C:/springWorkspace/fitnance_images/");
+				.addResourceLocations("file:///usr/local/project/fitnance_images/");
 	}
 
 }
